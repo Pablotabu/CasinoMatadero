3ae36eb0f61693b5fc799744ed6cbe9a486c18ab
pablotaboada <pablo.tabu@hotmail.com>
2018-09-04 20:44:35 +0200

    lee arrays con valores

--- js/game.js
+++ js/game.js
@@ -1,10 +1,14 @@
 function Game(canvasId) {
   this.canvas = document.getElementById(canvasId);
   this.ctx = this.canvas.getContext("2d");
+  this.myNumbers = [];
   this.reset();
   this.luckynumber = -1;
-  this.myNumbers = [];
 }
+Game.prototype.setBet = function(){
+    $(".bet").text(this.bet);
+}
+
 Game.prototype.start = function() {
   this.draw();
 };
@@ -18,29 +22,32 @@ Game.prototype.reset = function() {
       this.spinwheel.spin();
     }.bind(this)
   ); // "bind" hace que coja las propiedades anteriores al ultimo scope;
+
   
   $(".chips").on("click", function(e) {
     this.bet = e.currentTarget.value;
-    setBet(this.bet);
-  });
+    console.log(this)
+    this.setBet(this.bet);
+  }.bind(this));
+};
 
-  $(".number").on("click", function(e) {
-    this.myNumbers = (e.currentTarget.value);
-    console.log(this.myNumbers)
-    });
-    };
-
-function addNumber(number){
-    $(".number").text(number)
-}
 
 Game.prototype.draw = function() {
   this.spinwheel.draw();
 };
 
-function setBet(bet) {
-  $(".bet").text(bet);
+// function setBet(bet) {
+//   $(".bet").text(bet);
+// }
+
+Game.prototype.addNumber = function(number){
+  console.log(this.bet)
+   this.myNumbers.push([number,this.bet])
+
+   console.log(this.myNumbers)
 }
+
+
 
 // $(".number").on("click", function(e){
 //     console.log(e.currentTarget.value)
--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
 window.onload = function(){
     var game = new Game("wheelcanvas");
+    $(".number").on("click", function(e) {
+        var number = (e.currentTarget.value);
+        game.addNumber(number)
+         });
     function spin(){
         game.spinwheel.spin()
     }
